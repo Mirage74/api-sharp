4821f22a5e73dedc6f327870a37c48f1b3b4cf0d
balexvic <balexvicx@gmail.com>
2018-12-24 22:18:08 +0100

    heroku

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ module.exports  = {
     urlRest : "https://resize-img.herokuapp.com/",
     InputDir : "F:\\capitals\\src\\photos",
     OutputDir :  "F:\\capitals\\src\\photos\\res",
-    rotate : 0,
-    resize : 200,
-    width_Or_Height : "h"
+    rotate : 0, //666
+    resizeX : 80,
+    resizeY : 80,
+    useBothXY : "y", //if "y"/"Y" used both resizeX and resizeY, if not "y"/"Y", then below "x"/"X" (resizeX) or "y"/"Y" (resizeY)
+    width_Or_Height : "x"
 }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,24 @@
 const http = require('http')
 const sharp = require('sharp')
 
-
-// const app = new Koa();
-// const router = new Router();
-// app.use(serve('public'))
-// app.use(logger())
-// app.use(bodyParser())
-// //app.use(convert(body()))
-// app.use(router.routes())
-// //app.listen(process.env.PORT || 4000)
-// app.use(async (ctx, next) => {
-//     const origin = ctx.get('Origin');
-//     console.log(ctx.method)
-//     if (ctx.method !== 'OPTIONS') {
-//         ctx.set('Access-Control-Allow-Origin', origin);
-//         ctx.set('Access-Control-Allow-Credentials', 'true');
-//     } else if (ctx.get('Access-Control-Request-Method')) {
-//         ctx.set('Access-Control-Allow-Origin', origin);
-//         ctx.set('Access-Control-Allow-Methods', ['POST', 'OPTIONS']);
-//         ctx.set('Access-Control-Allow-Headers', 'Content-Type');
-//         ctx.set('Access-Control-Max-Age', '60');
-//         ctx.set('Access-Control-Allow-Credentials', 'true');
-//         ctx.response.status = 200
-//         console.log('ctx.response.status', ctx.response.status)
-//     }
-//     await next()
-// })
-
-
-
-
 const server = http.createServer(function(req, res) {
   let body = Buffer.alloc(0)
   const headers =  JSON.parse(JSON.stringify(req.headers))
   const widthOrHeight = headers['width-or-height'].toLowerCase()
   const rotate = parseInt(headers['rotate'])
-  let resize = parseInt(headers['resize'])
-  if (widthOrHeight === 'h') {
-      saveResize = resize
+  const resizeX = parseInt(headers['resizeX'])
+  const resizeY = parseInt(headers['resizeY'])
+  const useBothXY = headers['useBothXY'].toLowerCase()
+
+   let resize = resizeX
+  if ( (useBothXY !== 'y') && (widthOrHeight !== "x") ){
       resize = {}
-      resize.height = saveResize
+      resize.height = resizeY
   }
 
 
   req.on('data', (chunk) => {
-    console.log("chunk")
+    //console.log("chunk")
     let nextBuffer = Buffer.alloc(chunk.length, chunk)
     let arrBuf = [body, nextBuffer]
     body = Buffer.concat(arrBuf)
@@ -54,24 +27,29 @@ const server = http.createServer(function(req, res) {
 
 
   req.on('end', () => {
+    if (useBothXY === "y") {
       sharp(body)
-          .rotate(rotate)
-          .resize(resize, 100)
-
-          .toBuffer()
-          .then( data =>  {
-              res.end(data)
-              // const fdLog = fs.openSync("out2.jpg", "w")
-              // fs.appendFileSync(fdLog, data)
-              // fs.closeSync(fdLog)
-              // console.log("OK")
-          } )
-          .catch( err => {
-             console.log("Error sharp : ", err)
-          })
-
-
-    })
+        .rotate(rotate)
+        .resize(resizeX, resizeY)
+        .toBuffer()
+        .then( data =>  {
+          res.end(data)
+        })
+        .catch( err => {
+          console.log("Error sharp : ", err)
+        })
+    } else {
+      sharp(body)
+        .rotate(rotate)
+        .resize(resize)
+        .toBuffer()
+        .then( data =>  {
+          res.end(data)
+        })
+        .catch( err => {
+          console.log("Error sharp : ", err)
+        })}
+  })
 })
 
 
